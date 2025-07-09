31a614b6ee4ec799cf220c0a3a8cd241df74f8a9
z3r0265return <l1081684@outlook.com>
2025-07-08 23:16:58 -0400

    Update gash.c

--- gash.c
+++ gash.c
@@ -193,7 +193,7 @@ int parse_input(char *input, char **argv) {
                 continue;
             }
         }
-        argv[argc++] = token;
+        argv[argc++] = strdup(token);  // strdup here for all tokens because of the "free(): double free detected in tcache 2" error
         token = strtok(NULL, " ");
     }
     argv[argc] = NULL;
